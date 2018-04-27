864c7c4c9fba51299b7faa6775806ae0d186dd18
andrew wong <awwong92@gmail.com>
2018-04-27 12:52:29 -0400

    search field tied with local state

--- client/components/SearchBar.js
+++ client/components/SearchBar.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      findAccount: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e){
+    let santizedSearch = e.target.value.toLowerCase();
+    console.log(`${e.target.name} = ${santizedSearch}`)
+    this.setState({[e.target.name]: santizedSearch})
+  }
 
   render(){
     return (
-      <div>Hi, i'm the search bar.</div>
+      <input
+      name="findAccount"
+      onChange={this.handleChange}
+      placeholder="Type in a GitHub Account"
+      value={this.state.findAccount}
+    />
     )
   }
 }
